0937aaab732eca82c03aa9ae8690bc09511f588a
Juan Pablo <82411091+Juampy-cabj0@users.noreply.github.com>
2024-05-11 16:36:52 -0300

    Nuevos cambios
    Modifique para que la barra de navegacion desaparezca una vez que selecciono un enlace del menu

--- script/script.js
+++ script/script.js
@@ -3,3 +3,12 @@ const nav = document.querySelector(".barra");
 window.addEventListener("scroll", () => {
   nav.classList.toggle("active", window.scrollY > 100);
 });
+
+const menu = document.querySelector("#menu");
+const menuLinks = document.querySelectorAll(".menu a[href^='#']");
+
+menuLinks.forEach((menuLinks) => {
+  menuLinks.addEventListener("click", function () {
+    menu.click();
+  });
+});
