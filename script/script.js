dc4d641a30a23818f5d63fc118e1d8c54371ae11
Juan Pablo <82411091+Juampy-cabj0@users.noreply.github.com>
2024-05-16 16:14:37 -0300

    Agregados
    Agregue un boton para desplazarse hacia arriba al hacer scroll, le di funcionalidad con js, agregue el icono en la pestaña de navegacion, agregue un archivo "normalize" (para que la pagina funcione igual en todos los navegadores) y varios de los colores usados en css los puse dentro de un root, para que sea mas facil modificar los colores de la pagina

--- script/script.js
+++ script/script.js
@@ -1,14 +1,26 @@
-const nav = document.querySelector(".barra");
+function scroll() {
+  const nav = document.querySelector(".barra");
+  const flecha = document.querySelector(".go-up");
 
-window.addEventListener("scroll", () => {
-  nav.classList.toggle("active", window.scrollY > 100);
-});
-
-const menu = document.querySelector("#menu");
-const menuLinks = document.querySelectorAll(".menu a[href^='#']");
-
-menuLinks.forEach((menuLinks) => {
-  menuLinks.addEventListener("click", function () {
-    menu.click();
+  window.addEventListener("scroll", () => {
+    nav.classList.toggle("active", window.scrollY > 100);
   });
-});
+
+  window.addEventListener("scroll", () => {
+    flecha.classList.toggle("active", window.scrollY > 500);
+  });
+}
+
+function selectMenu() {
+  const menu = document.querySelector("#menu");
+  const menuLinks = document.querySelectorAll(".menu a[href^='#']");
+
+  menuLinks.forEach((menuLinks) => {
+    menuLinks.addEventListener("click", function () {
+      menu.click();
+    });
+  });
+}
+
+scroll();
+selectMenu();
