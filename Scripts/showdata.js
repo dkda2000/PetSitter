8e1bfe7866a6f5158743545e74824216c22154ff
dkda2000 <92997969+dkda2000@users.noreply.github.com>
2024-05-21 21:56:17 -0300

    forms
    forms

--- Scripts/showdata.js
+++ Scripts/showdata.js
@@ -1,8 +1,4 @@
-//let algunasPersonas = [{"nombre" :"eze", "apellido": "vel"}, {"nombre" : "juan", "apellido" : "perez"}];
 
-//llenarTabla(algunasPersonas);
-
-// Si se quiere tomar los datos que vienen de un form por url:
 capturarDatosPasadosEnLaURL();	
 function capturarDatosPasadosEnLaURL(){			
   let params = new URLSearchParams(window.location.search);	
@@ -16,30 +12,30 @@ function capturarDatosPasadosEnLaURL(){
   llenarTabla(resArray);
 }	
 
-	function llenarTabla(pedido){
+	function llenarTabla(contacto){
 
 		// Capturar la tabla en una variable
-		let laTabla = document.querySelector("#Pedidos");
+		let laTabla = document.querySelector("#Contacto");
     
-    laTabla.classList.add("tablapedidos")
+    laTabla.classList.add("tablacontactos")
 
-    let campos = Object.keys(pedido[0]); 
+    let campos = Object.keys(contacto[0]); 
 
     for(let i=0; i< campos.length; i++){
 
       let campo = campos[i];
-      let valorCampo = pedido[0][campo];
+      let valorCampo = contacto[0][campo];
       let row = document.createElement("tr");
       let celdaCampo = document.createElement("td");
 		  let celdaValor = document.createElement("td");
 
       //celdaCampo.style.cssText = 'color:red;background-color:yellow';
       
-      row.classList.add("filaspedidos")
-      celdaCampo.classList.add("celdaspedidos")
-      celdaCampo.classList.add("columnarightpedidos")
-      celdaValor.classList.add("columnaleftpedidos")
-      celdaValor.classList.add("celdaspedidos")
+      // row.classList.add("filaspedidos")
+      // celdaCampo.classList.add("celdaspedidos")
+      // celdaCampo.classList.add("columnarightpedidos")
+      // celdaValor.classList.add("columnaleftpedidos")
+      // celdaValor.classList.add("celdaspedidos")
 
 
       celdaCampo.innerText = campo;
